ba89a0511787ea72689f462869129d58f672624f
Adam Corriveau <39892162+livemonkey1300@users.noreply.github.com>
2024-11-21 12:42:01 +0000

    Done

--- startup.sh
+++ startup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Update package list and install python3-pip
-apt update && apt install -y python3-pip
+apt update && apt install -y python3-pip python3 python3.12-venv
 
-# Install Ansible using pip
-pip3 install ansible
+# Install the new environement 
+python3 -m venv ~/.venv
+source ~/.venv/bin/activate
+
+# Install the pip package 
+pip install ansible
 
 # Install the Ansible role for Docker
 ansible-galaxy role install geerlingguy.docker
